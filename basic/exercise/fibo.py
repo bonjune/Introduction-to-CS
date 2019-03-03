ccc08c20732c039f8d84f300ea8115ae9b32bd54
Bill Jang <malloc099@gmail.com>
2019-03-03 20:05:31 +0900

    Add forloop, and its exercise

---
+++ basic/exercise/fibo.py
@@ -0,0 +1,15 @@
+# Write a code to build fibonacci sequence
+
+# 0, 1, 1, 2, 3, 5, ...
+n = 10
+res = []
+
+for i in range(n):
+    # Declare a variable and assign a value for next element
+    # Then append it to list res
+    pass
+
+# For test
+
+for i, number in enumerate(res):
+    print(i+1, number)
---
+++ basic/whileloop.py
@@ -0,0 +1,4 @@
+'''
+While-loops are condition-based, which means it is suitable to solve problems where condition exists.
+'''
+
